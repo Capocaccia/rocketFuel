aa3d9fd80fbe6d7476a39812842c849e4963bced
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-11-15 13:52:58 -0600

    added jquery for placeholders and collecting input value of email/name fields

--- jscripts/main.js
+++ jscripts/main.js
@@ -1,3 +1,12 @@
  $(window).load(function() {
     $('.flexslider').flexslider();
   });
+
+$('#nameInput').attr('placeholder','NAME');
+$('#mailInput').attr('placeholder','EMAIL');
+
+$('#submit').on('click', function(){
+  var name = $('#nameInput').val();
+  var input = $('#mailInput').val();
+  //post request goes here for mailing plugin
+})
